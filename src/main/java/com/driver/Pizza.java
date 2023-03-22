474271084999c21e4fd4317debd4c3894292484f
saurabhraulkar <85070638+saurabhraulkar@users.noreply.github.com>
2023-03-22 11:51:52 +0530

    first commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,34 +1,101 @@
 package com.driver;
 
-public class Pizza {
+public class  Pizza {
 
-    private int price;
-    private Boolean isVeg;
-    private String bill;
+//    private int price;
+//    private Boolean isVeg;
+//    private String bill;
+    public int cheesePrice;
+    public int basePrice;
+    public int toppingPrice;
+    public int paperbagPrice;
+    public int totalprice;
+    public boolean ischessadded;
+    public boolean isToppingadded;
+    public boolean isPaperabackadded;
+    boolean isbillgenrated;
+    public  String bill;
 
     public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
+
         // your code goes here
+
+        if(isVeg){
+            basePrice=300;
+            toppingPrice=70;
+            this.totalprice= totalprice+basePrice+toppingPrice;
+
+        }
+        else{
+            basePrice=400;
+            toppingPrice=120;
+            this.totalprice= totalprice+basePrice+toppingPrice;
+        }
+        paperbagPrice=20;
+        cheesePrice=80;
+         ischessadded=false;
+         isToppingadded=false;
+         isPaperabackadded= false;
+        boolean isbillgenrated=false;
+        bill="Base Price Of The Pizza: "+basePrice+"\n";
+
     }
 
     public int getPrice(){
-        return this.price;
+
+        return totalprice;
     }
 
     public void addExtraCheese(){
         // your code goes here
+
+        if(ischessadded){
+            int a=20;
+        }
+        else{
+            ischessadded=true;
+            this.totalprice=totalprice+cheesePrice;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+
+        if(isToppingadded==false){
+            this.totalprice=totalprice+toppingPrice;
+            isToppingadded=true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(isPaperabackadded==false){
+            this.totalprice=totalprice+paperbagPrice;
+            isPaperabackadded=true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+
+        if(isbillgenrated==false){
+            isbillgenrated=true;
+
+            if(ischessadded==true){
+                bill=bill+"Extra Cheese Added: "+cheesePrice+"\n";
+            }
+            if(isToppingadded==true){
+                bill=bill+"Extra Toppings Added: "+toppingPrice+"\n";
+            }
+            if(isPaperabackadded==true){
+                bill=bill+"Paperbag Added: "+paperbagPrice+"\n";
+            }
+            bill=bill+"Total Price: "+totalprice+"\n";
+
+        }
+
+            return bill;
+
+
     }
 }
